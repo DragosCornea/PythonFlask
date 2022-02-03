d04a2b131d4a4d1ca238fb935e7f145980d84ec3
corneadragos89@gmail.com <corneadragos89@gmail.com>
2022-02-04 00:00:50 +0200

    AddingFolderStructure

--- website/__init__.py
+++ website/__init__.py
@@ -3,5 +3,6 @@ from flask import Flask
 def create_app(): # function-
     app =Flask(__name__) #__name__ name of the file that runs
     app.config['SECRET_KEY']='jajjajaja dsakjdaskjjk' #encrypt the data related to our website
-
     return app
+
+    
