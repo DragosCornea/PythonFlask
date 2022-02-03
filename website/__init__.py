3ba79cb7f400cf4e51c49c82a93dee661e6d70d4
corneadragos89@gmail.com <corneadragos89@gmail.com>
2022-02-04 00:39:18 +0200

    RestoreCode

--- website/__init__.py
+++ website/__init__.py
@@ -1,8 +1,11 @@
 from flask import Flask 
 
-def create_app(): # function-
-    app =Flask(__name__) #__name__ name of the file that runs
-    app.config['SECRET_KEY']='jajjajaja dsakjdaskjjk' #encrypt the data related to our website
-    return app
+app= Flask(__name__)
 
+@app.route('/')
+def index():
+    return "Hello"
+
+if __name__ =="__main__":
+    app.run(debug=True)
     
