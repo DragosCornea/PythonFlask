bd9a2dacc19c57fc51a2ac9933b72afba0e46378
corneadragos89@gmail.com <corneadragos89@gmail.com>
2022-02-04 03:32:48 +0200

    addingComments

--- website/__init__.py
+++ website/__init__.py
@@ -1,11 +1,13 @@
 from flask import Flask 
 
+#init app
 app= Flask(__name__)
 
 @app.route('/')
 def index():
     return "Hello"
 
+#run server
 if __name__ =="__main__":
     app.run(debug=True)
     
