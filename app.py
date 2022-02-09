425424ff6dc4a94ea4f97f337b1cd0d982977f3b
corneadragos89@gmail.com <corneadragos89@gmail.com>
2022-02-09 16:06:13 +0200

    FileArrangments

--- app.py
+++ app.py
@@ -1,12 +1,10 @@
 from flask import Flask, render_template
 
-#init app
-app= Flask(__name__)
+app=Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-#run server
 if __name__ =="__main__":
     app.run(debug=True)
